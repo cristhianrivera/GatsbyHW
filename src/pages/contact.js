e1dc1f57c44b75f7b5504343c17e732fe0c8b1e2
CristhianRivera <cristhianr.jimenez.rivera@gmail.com>
2020-12-29 13:37:46 +0100

    first commit

---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Header from  "../components/header"
+
+export default function Contacts() {
+  return (
+    <div style={{ color: `teal` }}>
+        <Header htext="This Is Contact"/>
+      <p>Starting with gatsby</p>
+    </div>
+  )
+}
